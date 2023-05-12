01253b1ee2f0bcfd8b3e08b1c89f880442e6d369
Micael-Macedo <micaelm2009@hotmail.com>
2023-05-12 09:05:04 -0300

    feat: add update index

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ error_reporting(E_ERROR | E_PARSE);
 include("./jwt/index.php");
 include_once("./database/mysql.php");
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
 $url = explode('/', $_GET['url']);
 
@@ -19,68 +21,65 @@ $autenticado = false;
 if ($header['Authorization'] != '') {
     if ($jwt->decode($header['Authorization']) != false) {
         $autenticado = true;
-    }else{
+    } else {
         $autenticado = false;
     }
 }
 
 if ($metodo === "GET") {
     if ($autenticado) {
-
     } else {
         switch ($page) {
-            case 'login':
-                // $body->senha = $jwt->base64encode($body->senha);
-                $token = $jwt->encode((array) $body);
-                $pessoa = json_decode(buscarUsuario($body->email));
-                $tokenUser = $jwt->encode($pessoa);
-                    echo  json_encode($token);
-                    $_COOKIE['nivelAcesso'] = $pessoa->nivelAcesso;
-
-                break;
-            case 'produtos':
-                echo buscarProduto($page);
-                break;
-            case 'logout':
-
-            default:
-                http_response_code(404);
-                break;
         }
     }
 }
 if ($metodo === "POST") {
-        switch ($page) {
-            case 'produto':
-                if ($_COOKIE['nivelAcesso'] >= 2) {
-                    $produto["nome"]= $body->nome;
-                    $produto["categoria"]= $body->categoria;
-                    $produto["fornecedor"]= $body->fornecedor;
-                    $produto["imagem"]= $body->imagem;
-                    $produto["localEstoque"]= $body->localEstoque;
-                    $produto["qtdDisponivel"]= $body->qtdDisponivel;
-                    $objeto = (object) $produto;
-                    echo createProduto($objeto);
-                }
-                break;
-            case 'feedback':
-                if ($_COOKIE['nivelAcesso'] == 1) {
-                    $avaliacao["usuario"]= $body->usuario;
-                    $avaliacao["produto"]= $body->produto;
-                    $avaliacao["comentario"]= $body->comentario;
-                    $avaliacao["classificacao"]= $body->classificacao;
-                    echo feedbackProduto($body->avaliacao);
-                }
+    switch ($page) {
+        case 'produto':
+            if ($_COOKIE['nivelAcesso'] >= 2) {
+                $produto["nome"] = $body->nome;
+                $produto["categoria"] = $body->categoria;
+                $produto["fornecedor"] = $body->fornecedor;
+                $produto["localEstoque"] = $body->localEstoque;
+                $produto["qtdDisponivel"] = $body->qtdDisponivel;
+                $objeto = (object) $produto;
+                echo createProduto($objeto);
+            }
+            break;
+        case 'feedback':
+            if ($_COOKIE['nivelAcesso'] == 1) {
+                $avaliacao["usuario"] = $body->usuario;
+                $avaliacao["produto"] = $body->produto;
+                $avaliacao["comentario"] = $body->comentario;
+                $avaliacao["classificacao"] = $body->classificacao;
+                echo feedbackProduto($body->avaliacao);
+            }
 
-                break;
-            case 'logout':
-                if ($_COOKIE['nivelAcesso'] >= 1) {
-                    $_COOKIE['nivelAcesso'] = 0;
-                }
-                break;
-            case 'usuario':
-                echo createUsuario($body);
+            break;
+        case 'logout':
+            if ($_COOKIE['nivelAcesso'] >= 1) {
+                $_COOKIE['nivelAcesso'] = 0;
+            }
+            break;
+        case 'login':
+            $body->senha = $jwt->base64encode($body->senha);
+            $token = $jwt->encode((array) $body);
+            $pessoa = json_decode(buscarUsuario($body->email));
+            $tokenUser = $jwt->encode($pessoa);
+            echo  json_encode($token);
+            $_COOKIE['nivelAcesso'] = $pessoa->nivelAcesso;
 
+            break;
+        case 'produtos':
+            echo buscarProduto($page);
+            break;
+        case 'logout':
+
+        case 'usuario':
+            echo createUsuario($body);
+        default:
+            http_response_code(404);
+            break;
     }
 }
 if ($metodo === "DELETE") {
